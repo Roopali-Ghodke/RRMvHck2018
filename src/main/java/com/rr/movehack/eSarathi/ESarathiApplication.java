1e76db0380d545b5b0c53c715a241f043b1cb1e5
Ravi Kharatmal <ravi.kharatmal@gmail.com>
2018-08-19 12:41:34 -0700

    Added MVC code.

--- src/main/java/com/rr/movehack/eSarathi/ESarathiApplication.java
+++ src/main/java/com/rr/movehack/eSarathi/ESarathiApplication.java
@@ -2,13 +2,20 @@ package com.rr.movehack.eSarathi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.rr.movehack.eSarathi"})
-public class ESarathiApplication {
+@ComponentScan(basePackages = { "com.rr.movehack.eSarathi" })
+public class ESarathiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ESarathiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ESarathiApplication.class);
 	}
 }
