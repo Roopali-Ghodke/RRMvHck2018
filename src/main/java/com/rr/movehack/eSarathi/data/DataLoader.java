b83dedd51ab1deb70d11e1e993ed888d89f593a3
Ravi Kharatmal <ravi.kharatmal@gmail.com>
2018-08-19 13:32:38 -0700

    Added data sheets

--- src/main/java/com/rr/movehack/eSarathi/data/DataLoader.java
+++ src/main/java/com/rr/movehack/eSarathi/data/DataLoader.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,64 +21,61 @@ public class DataLoader {
 
 	public Connection getConnection() throws Exception {
 
-		// String driver = "sun.jdbc.odbc.JdbcOdbcDriver";
-		// String driver ="org.hsqldb.jdbc.JDBCDriver";
 		String driver = "org.h2.Driver";
-		// String url = "jdbc:odbc:excelDB";
-		// String url = "jdbc:odbc:Driver={Microsoft Excel Driver (*.xls)};"
-		// +
-		// "DBQ=C:\\MSTD\\Codebases\\Movehack\\RRMvHck2018\\data\\bus\\Bus_Route_Trip_Data.csv";
-		// String url =
-		// "'jdbc:hsqldb:file:C:\\MSTD\\Codebases\\Movehack\\RRMvHck2018\\data\\bus\\Bus_Route_Trip_Data";
 		String url = "jdbc:h2:mem:testdb";
 		String username = "yourName";
 		String password = "yourPass";
 		Class.forName(driver);
-		// Class.forName("org.hsqldb.jdbcDriver");
 		return DriverManager.getConnection(url, username, password);
 	}
 
 	@PostConstruct
 	public void loadData() {
-		String excelQueryLoad = "CREATE TABLE BUS_ROUTE_TRIP AS SELECT * FROM CSVREAD('C:\\MSTD\\Codebases\\Movehack\\RRMvHck2018\\data\\bus\\Bus_Route_Trip_Data.csv');";
+		// String excelQueryLoad = "CREATE TABLE BUS_ROUTE_TRIP AS SELECT * FROM
+		// CSVREAD('C:\\MSTD\\Codebases\\Movehack\\RRMvHck2018\\data\\bus\\Bus_Route_Trip_Data.csv');";
+		// String excelQueryLoad = "CREATE TABLE BUS_ROUTE_TRIP AS SELECT * FROM
+		// CSVREAD('classpath:data\\bus\\Bus_Route_Trip_Data.csv');";
+		//
+		// jdbcTemplate.execute(excelQueryLoad);
+
+		loadCsvAsTable("data\\bus\\Bus_Route_Trip_Data.csv", "BUS_ROUTE_TRIP");
+		loadCsvAsTable("data\\metro\\DMRC_ENTRY_EXIT_20180722.csv", "METRO_ENTRY_EXIT");
+		loadCsvAsTable("data\\metro\\StationID_StationName_Mapping.xlsx", "METRO_STATION_ID");
+
+		// String excelQuery = "select * from BUS_ROUTE_TRIP";
+		// jdbcTemplate.query(excelQuery, new ResultSetExtractor<List>() {
+		//
+		// public List extractData(ResultSet resultSetObj) throws SQLException,
+		// DataAccessException {
+		// List editorList = new ArrayList();
+		// while (resultSetObj.next()) {
+		// System.out.println(resultSetObj.getString(1));
+		// }
+		// return editorList;
+		// }
+		//
+		// });
+	}
+
+	private void loadCsvAsTable(String csvPath, String tableName) {
+		// String excelQueryLoad = "CREATE TABLE BUS_ROUTE_TRIP AS SELECT * FROM
+		// CSVREAD('C:\\MSTD\\Codebases\\Movehack\\RRMvHck2018\\data\\bus\\Bus_Route_Trip_Data.csv');";
+		String excelQueryLoad = "CREATE TABLE " + tableName + " AS SELECT * FROM CSVREAD('classpath:" + csvPath + "');";
 
 		jdbcTemplate.execute(excelQueryLoad);
-
-		String excelQuery = "select * from BUS_ROUTE_TRIP";
+		String excelQuery = "select count(*) from " + tableName;
 		jdbcTemplate.query(excelQuery, new ResultSetExtractor<List>() {
 
 			public List extractData(ResultSet resultSetObj) throws SQLException, DataAccessException {
 				List editorList = new ArrayList();
 				while (resultSetObj.next()) {
-					System.out.println(resultSetObj.getString(1));
+					System.out
+							.println("############# LOADED = " + tableName + "  COUNT = " + resultSetObj.getString(1));
 				}
 				return editorList;
 			}
 
 		});
-	}
-
-	public void loadDataOld() throws Exception {
-
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-
-		conn = getConnection();
-		stmt = conn.createStatement();
-		String excelQueryLoad = "CREATE TABLE BUS_ROUTE_TRIP AS SELECT * FROM CSVREAD('C:\\MSTD\\Codebases\\Movehack\\RRMvHck2018\\data\\bus\\Bus_Route_Trip_Data.csv');";
-		stmt.executeUpdate(excelQueryLoad);
-
-		String excelQuery = "select * from BUS_ROUTE_TRIP";
-		rs = stmt.executeQuery(excelQuery);
-
-		while (rs.next()) {
-			System.out.println(rs.getString(1));
-		}
-
-		rs.close();
-		stmt.close();
-		conn.close();
 
 	}
 
