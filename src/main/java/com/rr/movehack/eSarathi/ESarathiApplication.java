bfb33d15fdccdd7f78b011987022d829d5634846
Ravi Kharatmal <ravi.kharatmal@gmail.com>
2018-08-18 22:32:04 -0700

    Initial project setup

---
+++ src/main/java/com/rr/movehack/eSarathi/ESarathiApplication.java
@@ -0,0 +1,12 @@
+package com.rr.movehack.eSarathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESarathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESarathiApplication.class, args);
+	}
+}
