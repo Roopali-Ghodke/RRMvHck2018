0b58615f3ade8c5305622561fb35f180246f27d3
Ravi Kharatmal <ravi.kharatmal@gmail.com>
2018-08-27 17:55:06 -0700

    Fixed test.

--- src/test/java/com/rr/movehack/smartyatricard/SmartYatriCardApplicationTests.java
+++ src/test/java/com/rr/movehack/smartyatricard/SmartYatriCardApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rr.movehack.eSarathi;
+package com.rr.movehack.smartyatricard;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ESarathiApplicationTests {
+public class SmartYatriCardApplicationTests {
 
 	@Test
 	public void contextLoads() {
